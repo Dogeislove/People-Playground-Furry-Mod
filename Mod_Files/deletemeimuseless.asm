d9c9a3b590cf05c52ad72a8fb5e2a5c957c49a5a
Dogeislove <48969373+Dogeislove@users.noreply.github.com>
2022-07-11 17:54:10 -0600

    Update deletemeimuseless.asm

--- Mod_Files/deletemeimuseless.asm
+++ Mod_Files/deletemeimuseless.asm
@@ -1,7 +1,12 @@
 ;heh funny commento
+;for some reason i'm creating this file thats absolutely useless
 global useless 
 
 section .text
 useless:
+        push ebp
         inc eax
+        pop ebp
         int 0x80
+        ret
+       
